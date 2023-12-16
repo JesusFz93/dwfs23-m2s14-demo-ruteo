eef0427a75e84fa0a7cab4990833b1f35a117bdd
Jesus <jesus.fdz93@gmail.com>
2023-12-16 13:11:30 -0600

    axios

--- src/pages/BrandsPage.jsx
+++ src/pages/BrandsPage.jsx
@@ -1,27 +1,37 @@
 import { useState } from "react";
+import axios from "axios";
 
-const marcas = [
-  {
-    id: "1",
-    name: "Mercedez",
-  },
-  {
-    id: "2",
-    name: "Toyota",
-  },
-  {
-    id: "3",
-    name: "Tesla",
-  },
-];
+// const marcas = [
+//   {
+//     id: "1",
+//     name: "Mercedez",
+//   },
+//   {
+//     id: "2",
+//     name: "Toyota",
+//   },
+//   {
+//     id: "3",
+//     name: "Tesla",
+//   },
+// ];
 
 const BrandsPage = () => {
-  const [brands, setBrands] = useState(marcas);
+  const [brands, setBrands] = useState([]);
+
+  const obtenerMarcas = async () => {
+    const resp = await axios.get("https://dwfs-api.onrender.com/api/v1/brands");
+    console.log(resp.data.data);
+    setBrands(resp.data.data);
+  };
 
   return (
     <>
       <header className="row col">
         <h1>Pagina de marcas</h1>
+        <button className="btn btn-info" onClick={obtenerMarcas}>
+          Obtener marcas
+        </button>
       </header>
       <main className="row">
         <article className="col">
@@ -29,7 +39,8 @@ const BrandsPage = () => {
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
+                <th scope="col">Name</th>
+                <th scope="col">Desde</th>
               </tr>
             </thead>
             <tbody>
@@ -37,7 +48,15 @@ const BrandsPage = () => {
                 return (
                   <tr key={brand.id}>
                     <th scope="row">{brand.id}</th>
-                    <td>{brand.name}</td>
+                    <td>{brand.nombre}</td>
+                    <td>{brand.desde}</td>
+                    <td>
+                      <img
+                        className="img-fluid"
+                        src={brand.imagen}
+                        alt={brand.nombre}
+                      />
+                    </td>
                   </tr>
                 );
               })}
